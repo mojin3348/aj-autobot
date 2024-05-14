7f1ebec592626ad5f661b73e512c92d6b75f49d9
scyte000 <scyte000@gmail.com>
2024-05-14 23:18:43 +0800

    Update sendnoti.js

--- script/sendnoti.js
+++ script/sendnoti.js
@@ -1,78 +1,78 @@
 const axios = require("axios");
-const { createReadStream, unlinkSync } = require("fs");
-const { resolve } = require("path");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports.config = {
-  name: "sendnoti",
-  version: "1.1.0",
-  role: 2,
-  credits: "cliff",
-  description: "Sends a message to all groups and can only be done by the admin.",
-  aliases: ["noti"],
-  cooldown: 0,
-  hasPrefix: false,
-  usage: "",
+	name: "sendnoti",
+	version: "1.1.0",
+	role: 2,
+	description: "Sends a message to all groups and can only be done by the admin.",
+	hasPrefix: false,
+	aliases: ["noti"],
+	usages: "[Text]",
+	cooldown: 0,
 };
 
-module.exports.run = async function ({ api, event, args }) {
-  if (this.config.credits !== "cliff") {
-    return api.sendMessage(
-      `[ 𝗔𝗡𝗧𝗜 𝗖𝗛𝗔𝗡𝗚𝗘 𝗖𝗥𝗘𝗗𝗜𝗧𝗦 ]
-𝗔𝗗𝗠𝗜𝗡 𝗠𝗘𝗦𝗦𝗔𝗚𝗘: 
-ᴄʜᴀɴɢᴇ ᴄʀᴇᴅɪᴛs ᴘᴀ ᴀᴋᴏ sᴀʏᴏ ᴍᴀɢ ᴘʀᴀᴄᴛɪᴄᴇ ᴋᴀ😝 
-𝗠𝗘𝗠𝗕𝗘𝗥 𝗠𝗘𝗦𝗦𝗔𝗚𝗘:
-𝚃𝚑𝚒𝚜 𝚋𝚘𝚝 𝚌𝚛𝚎𝚊𝚝𝚘𝚛 𝚒𝚜 𝚊 𝚌𝚑𝚊𝚗𝚐𝚎 𝚌𝚛𝚎𝚍𝚒𝚝𝚘𝚛 𝚔𝚊𝚢𝚊 𝚋𝚎 𝚊𝚠𝚊𝚛𝚎 𝚗𝚎𝚡𝚝 𝚝𝚒𝚖𝚎.
+module.exports.run = async function ({ api, event, args, admin }) {
+	const threadList = await api.getThreadList(100, null, ["INBOX"]);
+	let sentCount = 0;
+	const custom = args.join(" ");
 
-𝗢𝗪𝗡𝗘𝗥 𝗢𝗙 𝗧𝗛𝗜𝗦 𝗖𝗢𝗠𝗠𝗔𝗡𝗗: 
-https://facebook.com/100053549552408`,
-      event.threadID,
-      event.messageID
-    );
-  }
+	async function sendMessage(thread) {
+		try {
+			await api.sendMessage(
+`🟢🟡🔴\n ----------------\n『 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍 』\n\n ----------------\n𝑴𝒆𝒔𝒔𝒂𝒈𝒆 𝒇𝒓𝒐𝒎 𝒂𝒅𝒎𝒊𝒏:「${custom}」\n _________________________`,
+				thread.threadID
+			);
+			sentCount++;
 
-  const threadList = await api.getThreadList(25, null, ["INBOX"]);
-  let sentCount = 0;
-  const custom = args.join(" ");
+			const content = `${custom}`;
+			const languageToSay = "tl"; 
+			const pathFemale = path.resolve(__dirname, "cache", `${thread.threadID}_female.mp3`);
 
-  async function sendMessage(thread) {
-    try {
-      await api.sendMessage(
-        `📣ᴍᴇꜱꜱᴀɢᴇ ꜰʀᴏᴍ ᴀᴅᴍɪɴ:\n---------\n${custom}`,
-        thread.threadID
-      );
-      sentCount++;
+			await downloadFile(
+				`https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(content)}&tl=${languageToSay}&client=tw-ob&idx=1`,
+				pathFemale
+		);
+			api.sendMessage(
+				{ attachment: fs.createReadStream(pathFemale) },
+				thread.threadID,
+				() => fs.unlinkSync(pathFemale)
+			);
+		} catch (error) {
+			console.error("Error sending a message:", error);
+		}
+	}
 
-      const content = `${custom}`;
-      const languageToSay = "tl";
-      const pathFemale = resolve(__dirname, "cache", `${thread.threadID}_female.mp3`);
+	for (const thread of threadList) {
+		if (sentCount >= 20) {
+			break;
+		}
+		if (thread.isGroup && thread.name != thread.threadID && thread.threadID != event.threadID) {
+			await sendMessage(thread);
+		}
+	}
 
-      await global.utils.downloadFile(
-        `https://translate.google.com/translate_tts?ie=UTF-8&q=${encodeURIComponent(
-          content
-        )}&tl=${languageToSay}&client=tw-ob&idx=1`,
-        pathFemale
-      );
-      api.sendMessage({ attachment: createReadStream(pathFemale) }, thread.threadID, () => unlinkSync(pathFemale));
-    } catch (error) {
-      console.error("Error sending a message:", error);
-    }
-  }
-
-  for (const thread of threadList) {
-    if (sentCount >= 20) {
-      break;
-    }
-    if (thread.isGroup && thread.name != thread.threadID && thread.threadID != event.threadID) {
-      await sendMessage(thread);
-    }
-  }
-
-  if (sentCount > 0) {
-    api.sendMessage(`› Sent the notification successfully.`, event.threadID);
-  } else {
-    api.sendMessage(
-      "› No eligible group threads found to send the message to.",
-      event.threadID
-    );
-  }
+	if (sentCount > 0) {
+		api.sendMessage(`› Sent the notification successfully.`, event.threadID);
+	} else {
+		api.sendMessage(
+			"› No eligible group threads found to send the message to.",
+			event.threadID
+		);
+	}
 };
+
+async function downloadFile(url, filePath) {
+	const writer = fs.createWriteStream(filePath);
+	const response = await axios({
+		url,
+		method: 'GET',
+		responseType: 'stream'
+	});
+	response.data.pipe(writer);
+	return new Promise((resolve, reject) => {
+		writer.on('finish', resolve);
+		writer.on('error', reject);
+	});
+}
